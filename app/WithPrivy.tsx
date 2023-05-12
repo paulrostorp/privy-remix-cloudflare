847d12418e71bc1d2d7ffd7a3fdc8a2d0dec4ff1
Paul Rostorp <paul.rostorp@gmail.com>
2023-05-12 10:23:42 +0200

    add privy

---
+++ app/WithPrivy.tsx
@@ -0,0 +1,35 @@
+import { PrivyProvider } from "@privy-io/react-auth";
+import type { User } from "@privy-io/react-auth";
+import React from "react";
+
+const handleLogin = (user: User, isNewUser: boolean) => {
+  console.log("Login", user, isNewUser);
+};
+
+
+interface IWithPrivyProps {
+  children: React.ReactNode
+}
+
+export const WithPrivy = ({ children }: IWithPrivyProps): JSX.Element => {
+  if (typeof document !== "undefined") {
+    return <PrivyProvider
+      appId={"clhhtxurv000uld08vecuzqrq"}
+      onSuccess={handleLogin}
+      config={{
+        loginMethods: ["email", "google", "twitter", "wallet"],
+
+        appearance: {
+          theme: "dark",
+          accentColor: "#676FFF",
+          logo: "https://your-logo-url",
+          showWalletLoginFirst: false,
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  }
+  
+  return <>{children}</>
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
+import { usePrivy } from "@privy-io/react-auth";
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { WithPrivy } from "~/WithPrivy";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
@@ -8,33 +10,36 @@ export default function Index() {
   // const privy = usePrivy()
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <WithPrivy>
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+        <h1>Welcome to Remix</h1>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              href="https://remix.run/tutorials/blog"
+              rel="noreferrer"
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://remix.run/tutorials/jokes"
+              rel="noreferrer"
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        {/* <button onClick={privy.login}>Log in</button>; */}
+      </div>
+    </WithPrivy>
   );
 }
